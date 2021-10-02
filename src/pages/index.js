e598b0fe55eab739aff98725ba6c0781a92599d3
piano1029 <46605242+piano1029@users.noreply.github.com>
2021-10-02 11:37:21 +0200

    Fix website meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Clanny System's Official website!">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
