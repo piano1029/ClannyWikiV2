1417ed416b0b1d8c7c823f1dddefc8feee04d870
piano1029 <46605242+piano1029@users.noreply.github.com>
2021-10-02 11:32:01 +0200

    Fix edit this page urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,13 +22,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+          editUrl: 'https://github.com/piano1029/ClannyWikiV2/edit/main/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
